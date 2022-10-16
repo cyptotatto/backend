ca9559a07f73d8a45e78db46ca328ef783beeba5
hye-on <ain0103@naver.com>
2022-10-17 00:12:04 +0900

    .

--- src/api/routes/create.js
+++ src/api/routes/create.js
@@ -4,7 +4,9 @@ var createController = require('../../controllers/createController');
 //var nftController = require('../../controllers/NftController');
 
 //NFT만들기
-router.post('/create', createController.createNFT.create())//,(req, res) => {
+router.post('/create', createController.createNFT.create());
+//router.post('/create/사진', createController.createNFT.create());//,(req, res) => {
+//,(req, res) => {
 //return ;
 //});
 
