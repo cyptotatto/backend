2b6d9453564a4f714eba09931b8481765f593f2b
INBUM02 <kib0202@naver.com>
2022-11-02 23:55:42 +0900

    refact

--- src/api/controllers/detailController.js
+++ src/api/controllers/detailController.js
@@ -1,64 +1,47 @@
-var userService = require('../services/userService');  
+var userService = require('../services/userService');
 var nftService = require('../services/nftService');
 var transactionService = require('../services/transactionService');
- 
 
-module.exports = { 
-    getArtistDetail: async (req, res) => {
-        try{
-       
-        const userAccount=req.params.name;
-        //console.log(userAccount);
-        const ownedNft =await nftService.getOwnNFT(userAccount);
-        const madeNft =await nftService.getMadeNFT(userAccount);
-        const soldNft =await transactionService.getSoldNFT(userAccount);
-        const likeNft =await likeItemService.getLikeNFT(userAccount);
+module.exports = {
+  getArtistDetail: async (req, res) => {
+    try {
+      const userAccount = req.params.name;
+      //console.log(userAccount);
+      const ownedNft = await nftService.getOwnNFT(userAccount);
+      const madeNft = await nftService.getMadeNFT(userAccount);
+      const soldNft = await transactionService.getSoldNFT(userAccount);
+      const likeNft = await likeItemService.getLikeNFT(userAccount);
 
- 
-        
-       return res.status(200).json({
-            status:200,
-            message: "detail 가져오기 성공",
-           // data: nftRanking
-           ownedNFT :ownedNft ,
-            madeNFT :madeNft, 
-           soldNft: soldNft,
-          likeNTF: likeNft
-          
-        });
-    }catch(err)
-    {
-        console.log(err);
-        throw err;
+      return res.status(200).json({
+        status: 200,
+        message: 'detail 가져오기 성공',
+        // data: nftRanking
+        ownedNFT: ownedNft,
+        madeNFT: madeNft,
+        soldNft: soldNft,
+        likeNTF: likeNft,
+      });
+    } catch (err) {
+      console.log(err);
+      throw err;
     }
-           
-    },
+  },
 
-    getNftDetail: async (req, res) => {
-        try{
-       
-        const nftId=req.params.nftId;
-        
-        const nft =await nftService.getNFT(nftId);//nft이름 변경?
-        
+  getNftDetail: async (req, res) => {
+    try {
+      const nftId = req.params.nftId;
 
- 
-        
-       return res.status(200).json({
-            status:200,
-            message: "detail 가져오기 성공",
-           // data: nftRanking
-           nftId : nft ,
-            
-        });
-    }catch(err)
-    {
-        console.log(err);
-        throw err;
+      const nft = await nftService.getNFT(nftId); //nft이름 변경?
+
+      return res.status(200).json({
+        status: 200,
+        message: 'detail 가져오기 성공',
+        // data: nftRanking
+        nftId: nft,
+      });
+    } catch (err) {
+      console.log(err);
+      throw err;
     }
-           
-    }
-
-
-    
-}
+  },
+};
--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import express from "express";
-import config from "./config/index.js";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import morgan from "morgan";
-import helmet from "helmet";
-import { connectDB } from "./loaders/mongoose.js";
+import express from 'express';
+import config from './config/index.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import { connectDB } from './loaders/mongoose.js';
 // import indexRouter from "./api/routes/index.js";
 
 const app = express();
@@ -16,8 +16,8 @@ const corsOptions = {
 app.use(express.json()); // REST API, Body
 app.use(express.urlencoded({ extended: true })); // HTML Form => Body
 app.use(cookieParser()); // cookie 사용시 설정
-app.use(express.static("public")); // option도 가능
-app.use(morgan("tiny")); // winston
+app.use(express.static('public')); // option도 가능
+app.use(morgan('tiny')); // winston
 app.use(helmet()); //보안 이슈
 app.use(cors(corsOptions));
 
@@ -25,10 +25,10 @@ app.use(cors(corsOptions));
 
 app.use((error, req, res, next) => {
   console.log(error);
-  res.status(500).json({ message: "Something went wrong" });
+  res.status(500).json({ message: 'Something went wrong' });
 });
 
 connectDB().then(() => {
-  console.log("DB Connection Success!");
+  console.log('DB Connection Success!');
   const server = app.listen(config.host.port);
 });
