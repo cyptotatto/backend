23220a29c3e878f6252624b5fc85e41ca66e30dd
Hayoung0505 <s651210@naver.com>
2022-11-08 22:15:12 +0900

    FEAT: detail conflict resolve

--- src/api/routes/detail.js
+++ src/api/routes/detail.js
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
 import express from 'express';
 let router = express.Router();
-import detailController from '../controllers/detailController.js';
-=======
-const express = require('express');
-const router = express.Router();
-const detailController = require('../../controllers/detail.controller');
->>>>>>> e2e632f (feat: Detail,Create logic)
+import detailController from '../controllers/detail.controller.js';
 
 router.get('/nft', detailController.getNftDetail);
 
 router.get('/artist', detailController.getArtistDetail);
 
-<<<<<<< HEAD
-module.exports = router;
-=======
+
 router.get('/nft',detailController.getNftDetail); 
  
 
@@ -23,4 +15,3 @@ router.get('/artist',detailController.getArtistDetail);
 router.get('/buy',detailController.getBuy);
 
 module.exports = router;
->>>>>>> e2e632f (feat: Detail,Create logic)
