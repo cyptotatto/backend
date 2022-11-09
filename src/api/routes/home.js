875e6192fb6f6ffd22dc92f87eeb50c54484303d
Hayoung0505 <s651210@naver.com>
2022-11-10 01:09:35 +0900

    FEAT: home.js 수정

--- src/api/routes/home.js
+++ src/api/routes/home.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import express from 'express';
 const homeRouter= express.Router();
 //let rankingController =require('../controllers/ranking.controller.js');
-=======
-const express = require('express');
-const router = express.Router();
-// var userController = require('../../controllers/UserController');
-// var nftController = require('../../controllers/NftController');
-var rankingController = require('../../controllers/ranking.controller');
->>>>>>> e2e632f (feat: Detail,Create logic)
+
 
 import * as rankingController from '../controllers/ranking.controller.js';
 
