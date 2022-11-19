9173d08c2163166a696c8797bad64c97d7ef50fc
Hayoung0505 <s651210@naver.com>
2022-11-20 00:34:03 +0900

    FEAT: s3client refactoring

--- src/api/routes/create.js
+++ src/api/routes/create.js
@@ -5,26 +5,15 @@ import AWS  from 'aws-sdk';
 import multer  from 'multer';
 import multerS3  from 'multer-S3';
 import path  from 'path';
-import dotenv from 'dotenv';
+import s3 from '../../common/s3.js';
  
 
 const createRouter= express.Router();
 
 import * as createController from '../controllers/create.controller.js';
 
-dotenv.config();
-
-AWS.config.update({
-
-region: process.env.REGION,
-accessKeyId: process.env.S3_KEYID,
-secretAccessKey: process.env.S3_PRIVATE_KEY,
-
- 
-});
 //이부분은 따로 파일만들어서 export해서 multer기능 있는 파일에 s3를 import하는걸로 변경해야 함.
 
-const s3 = new AWS.S3();
 
 const allowedExtensions = ['.jpg','.png', '.GIF', '.WEBP', '.MP4', '.MP3'];
 
