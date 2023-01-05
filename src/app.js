029c124ea258206243babd5741a09f049c65459b
hye-on <ain0103@naver.com>
2023-01-05 23:35:20 +0900

    fix

--- src/app.js
+++ src/app.js
@@ -16,8 +16,12 @@ const corsOptions = {
   credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
 };
 
-app.use(express.json()); // REST API, Body
-app.use(express.urlencoded({ extended: true})); // HTML Form => Body
+app.use(express.json(
+  {limit : "50mb"}
+)); // REST API, Body
+app.use(express.urlencoded({ 
+  limit:"50mb",
+  extended: true})); // HTML Form => Body
 app.use(cookieParser()); // cookie 사용시 설정
 app.use(express.static('public')); // option도 가능
 app.use(morgan('tiny')); // winston
