01ff0f8568e08874458898a1f88703ca21d6b6bc
Hayoung0505 <s651210@naver.com>
2023-03-16 00:14:50 +0900

    Feat: cors옵션변경

--- src/app.js
+++ src/app.js
@@ -11,8 +11,11 @@ import router from './api/routes/index.js';
 
 const app = express();
 
+
+
 const corsOptions = {
-  origin: config.host.cors,// 접근 권한을 부여하는 도메인 
+  origin: 'http://localhost:3000',
+  //origin: config.host.cors,// 접근 권한을 부여하는 도메인 
   credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
 };
 
