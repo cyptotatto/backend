8aa6f4c841515988c4af28f8e07a09491befabb3
Hayoung0505 <s651210@naver.com>
2023-04-05 10:10:30 +0900

    Feat: 몽구즈 retry 예외로직 추가

--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -1,19 +1,23 @@
 import mongoose from 'mongoose';
 import config from '../config/index.js';
 
+const RETRY_INTERVAL = 5000; // 5초
+
 export async function connectDB() {
-  return mongoose.connect(config.db.url, {
-    dbName: 'chichi',
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  try {
+    await mongoose.connect(config.db.url, {
+      dbName: 'chichi',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('DB Connection Success!');
+  } catch (error) {
+    console.error('mongoose connect error', error);
+    setTimeout(() => connectDB(), RETRY_INTERVAL);
+  }
 }
 
-mongoose.connection.on('error', error => {
-  console.error('mongoose connect error', error);
-});
-
 mongoose.connection.on('disconnected', () => {
-  console.error('mongoose connect disconnected. retry to connnect');
-  connectDB();
+  console.error('mongoose connect disconnected. retry to connect');
+  setTimeout(() => connectDB(), RETRY_INTERVAL);
 });
